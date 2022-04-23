16e212b04fb462d1bf76febdf3505a93a66e4e78
Faruq Jefri <faruqjefri1998@gmail.com>
2022-04-24 04:22:00 +0800

    Initial commit
    Used bootstrap components

---
+++ resources/views/livewire/components/cart.blade.php
@@ -0,0 +1,78 @@
+<div class="container">
+        <h1>POS Cashier</h1>
+    
+        <div class="row">
+            <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Product</th>
+                    <th scope="col">Price (RM)</th>
+                    <th scope="col">Quantity</th>
+                    <th scope="col">Cost (RM)</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th>Product 1</th>
+                    <td>RM0.00</td>
+                    <td>1</td>
+                    <td>RM0.00</td>
+                  </tr>
+                  <tr>
+                    <th>Product 2</th>
+                    <td>RM0.00</td>
+                    <td>1</td>
+                    <td>RM0.00</td>
+                  </tr>
+                  <tr>
+                    <th>Product 3</th>
+                    <td>RM0.00</td>
+                    <td>1</td>
+                    <td>RM0.00</td>
+                  </tr>
+                  <tr>
+                    <th>Product 4</th>
+                    <td>RM0.00</td>
+                    <td>1</td>
+                    <td>RM0.00</td>
+                  </tr>
+                </tbody>
+              </table>
+        </div>
+        <div class="row">
+            <table class="table table-bordered">
+                <tbody>
+                  <tr>
+                    <th>Subtotal</th>
+                    <td>subtotal</td>
+                  </tr>
+                  <tr>
+                    <th>No. of items</th>
+                    <td>no of items</td>
+                  </tr>
+                  <tr>
+                    <th>Tax</th>
+                    <td>tax</td>
+                  </tr>
+                  <tr>
+                    <th>Service Charge</th>
+                    <td>service charge</td>
+                  </tr>
+                  <tr>
+                    <th>Total</th>
+                    <td>total</td>
+                  </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="row">
+            <div class="d-flex justify-content-end">
+                <div class="col-10 text-end">
+                    <button type="button" class="btn btn-danger">Clear</button>
+                </div>
+                <div class="col-2 text-end">
+                    <button type="button" class="btn btn-primary">Checkout</button>
+                </div>
+            </div>
+        </div>
+</div>
